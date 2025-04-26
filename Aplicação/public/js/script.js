6dad111499cf57fc1eaf92028b412bc026ba7423
EzequielC-Dev <ezequiel34324@gmail.com>
2025-04-26 19:45:56 -0300

    feat: adição do expanded na barra lateral

---
+++ Aplicação/public/js/script.js
@@ -0,0 +1,28 @@
+function expanded() {
+    const menu = document.querySelector('.principal-barra');
+
+    const home = document.querySelector('#principal-barra-home');
+    const historia = document.querySelector('#principal-barra-historia');
+    const personagens = document.querySelector('#principal-barra-personagens');
+    const forum = document.querySelector('#principal-barra-forum');
+
+
+    menu.addEventListener("click", () => {
+        menu.classList.toggle("menu-ativo");
+        if(menu.classList.contains("menu-ativo")) {
+                    
+        home.innerHTML += "<p>Home</p>"
+        historia.innerHTML += "<p>História</p>"
+        personagens.innerHTML += "<p>Personagens</p>"
+        forum.innerHTML += "<p>Fórum</p>"
+        }
+        else {
+        home.innerHTML = `<a href="" id="barra-ativo"><img src="./assets/icons/icon-home.svg" alt="Home"></a> `; 
+        historia.innerHTML = `<a href=""><img src="./assets/icons/icon-historia.svg" alt="História"></a>`;
+        personagens.innerHTML = `<a href=""><img src="./assets/icons/icon-personagens.svg" alt="Personagens"></a>`;
+        forum.innerHTML = ` <a href=""><img src="./assets/icons/icon-forum.svg" alt="Fórum"></a>`;
+        }
+
+    })
+}
+
